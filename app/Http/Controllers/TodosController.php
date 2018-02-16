6933e541bcaf34d570be08ed9e14ca3c0706f6f1
sinyadal <v0xe7y2cl@protonmail.com>
2018-02-16 15:23:22 +0800

    Display data to views

--- app/Http/Controllers/TodosController.php
+++ app/Http/Controllers/TodosController.php
@@ -3,8 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Todo;
 
 class TodosController extends Controller
 {
-    //
+    public function index() 
+    {
+        $todos = Todo::all();
+                
+        return view('todos')
+            ->with(compact('todos'));
+    }
 }
